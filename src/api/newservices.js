13a78c64d83ef576abce6b9c50bda10a11a38a0c
ubaiddevpk <obaidullah.developer@gmail.com>
2025-09-08 16:45:04 +0500

    api updated

--- src/api/newservices.js
+++ src/api/newservices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://fluxtonx-dashboard-backend-production-0888.up.railway.app/api/newservices"; // update if your backend uses a different port
+const API_URL = "https://fluxtonx-dashboard.onrender.com/api/newservices"; // update if your backend uses a different port
 
 export const getnewServices = async () => {
   const res = await axios.get(API_URL);
--- src/api/projectsapi.js
+++ src/api/projectsapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://fluxtonx-dashboard-backend-production-0888.up.railway.app/api/projects"; // update if your backend uses a different port
+const API_URL = "https://fluxtonx-dashboard.onrender.com/api/projects"; // update if your backend uses a different port
 
 export const getprojects = async () => {
   const res = await axios.get(API_URL);
